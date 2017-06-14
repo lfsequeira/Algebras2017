416cfad4d6af28a35acc2ced159bfb2d87496423
Luis Sequeira <lfsequeira@fc.ul.pt>
2017-06-14 17:16:44 +0100

    Initial Commit

---
+++ Algebra2017/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Algebra2017
+//
+//  Created by Luis Sequeira on 14/06/2017.
+//  Copyright © 2017 Luis Sequeira. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
